afdb96b724cb449378960db70004c8433f4d6834
Emiliano Santarelli <e.santarelli@reply.it>
2024-02-07 17:39:13 +0100

    new Security with JWT v2

--- demo1/demo1/src/main/java/com/example/demo1/Controller/AuthController.java
+++ demo1/demo1/src/main/java/com/example/demo1/Controller/AuthController.java
@@ -1,75 +1,30 @@
 package com.example.demo1.Controller;
 
-import com.example.demo1.Model.Login;
-import com.example.demo1.Model.Role;
-import com.example.demo1.Model.SignUp;
-import com.example.demo1.Model.Utente;
-import com.example.demo1.Repository.RoleRepository;
-import com.example.demo1.Repository.UtenteRepo;
-import com.example.demo1.Service.impl.RoleServiceImpl;
-import com.example.demo1.Service.impl.UtenteServiceImpl;
+import com.example.demo1.Model.AuthenticationRequest;
+import com.example.demo1.Model.AuthenticationResponse;
+import com.example.demo1.Model.RegisterRequest;
+import com.example.demo1.Service.impl.AuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
-
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UtenteServiceImpl utenteService;
-    @Autowired
-    private RoleServiceImpl roleService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/signin")
-    public ResponseEntity<String> authenticateUser(@RequestBody Login login) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                login.getUsernameOrEmail(), login.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    private final AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody SignUp signUp) {
-
-        if (utenteService.existsByUsername(signUp.getUsername())) {
-            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        if (utenteService.existsByEmail(signUp.getEmail())) {
-            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        Utente user = new Utente();
-        user.setName(signUp.getName());
-        user.setUsername(signUp.getUsername());
-        user.setEmail(signUp.getEmail());
-        user.setPassword(passwordEncoder.encode(signUp.getPassword()));
-
-        Role roles = roleService.findByName("ROLE_ADMIN").get();
-        user.setRoles(Collections.singleton(roles));
-
-        utenteService.saveUtente(user);
-
-        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
-
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticateRequest(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
-
 }
